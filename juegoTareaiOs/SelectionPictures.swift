40ed97118434c6011203c12cc26c1825488c2743
Lorena <lorena_miguel_perez_m-tsapp1ma2324@cev.com>
2024-11-14 14:34:15 +0100

    Aparecen imagenes de nuestro array de fotos en las celdas. No aparecen los botones todavia.

--- juegoTareaiOs/SelectionPictures.swift
+++ juegoTareaiOs/SelectionPictures.swift
@@ -6,48 +6,72 @@
 //
 import UIKit
 
-class SelectionPictures: UIViewController{
-    
-   
+class SelectionPictures: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate{
+
     @IBOutlet weak var ColorsCollectionV: UICollectionView!
     
     
     // Array de imágenes para la galería
-    var photos: [UIImage] = []
-    
+
+    var photos = [
+        UIImage(named: "amarillo")!,
+        UIImage(named: "azul")!,
+        UIImage(named: "rojo")!,
+        UIImage(named: "azulclaro")!,
+        UIImage(named: "degradado")!,
+        UIImage(named: "verde")!,
+        UIImage(named: "naranja")!,
+        UIImage(named: "morado")!,
+        UIImage(named: "rosado")!,
+        UIImage(named: "verdeclaro")!
+    ]
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Las cargamos
-        photos = [
-            UIImage(named: "amarillo")!,
-            UIImage(named: "azul")!,
-            UIImage(named: "rojo")!,
-            UIImage(named: "azulclaro")!,
-            UIImage(named: "degradado")!,
-            UIImage(named: "verde")!,
-            UIImage(named: "naranja")!,
-            UIImage(named: "morado")!,
-            UIImage(named: "rosado")!,
-            UIImage(named: "verdeclaro")!
-        ]
+    
+        
+        ColorsCollectionV.dataSource = self
+        ColorsCollectionV.delegate = self
+        
+    }
+    /*
+     Este método define cuántas celdas habrá en nuestro CollectionView
+     */
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return photos.count
     }
     
-    // Este método define cuántas celdas habrá en el UICollectionView
-        func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-            return photos.count
-        }
-        
-        // Este método configura cada celda
-        func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-            // Obtén la celda reutilizable
-            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PhotoCell", for: indexPath) as! PhotoCell
-            
-            // Configura la celda con la imagen correspondiente
-            let image = photos[indexPath.item]
-            cell.CellimageV.image = image
-            
-            return cell
-        }
-         
-    
+    /*
+     Este método configura cada celda
+     */
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PhotoCell", for: indexPath) as! PhotoCell
+        cell.CellimageV.image = photos[indexPath.item]
+        print("\(indexPath.item)")
+        return cell
+    }
 }
+
+
+
+
+
+
+
+
+/*
+ func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+     return photos.count
+ }
+ 
+ 
+ func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+     // Obtén la celda reutilizable
+     let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PhotoCell", for: indexPath) as! PhotoCell
+     
+     // Configura la celda con la imagen correspondiente
+     let image = photos[indexPath.item]
+     cell.CellimageV.image = image
+     
+     return cell
+ }
+ */
